20bd09438cf54914f191c30dd79ce650db25aed2
nsm331 <aazz201591@gmail.com>
2025-11-27 23:50:48 +0300

    task3

---
+++ task3/lib/Searchpage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Searchpage extends StatelessWidget {
+  const Searchpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.home,size: 30,color: Colors.grey,),
+          SizedBox(height: 20,),
+          Text('البحث',style: TextStyle(fontSize: 24,color: Colors.grey),)
+        ],
+      ),
+    );
+  }
+}
+
